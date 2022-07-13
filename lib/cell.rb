6f40117f108ce2c1ed4a65fec47bafe47fc188c2
Shawn Lee <Shawnl93@hotmail.com>
2022-07-12 21:40:54 -0600

    feat: changed ship method and added place_ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,19 +2,18 @@ require './lib/ship'
 
 class Cell
 
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
-  end
-
-  def ship
-
+    @ship = nil
   end
 
   def empty?
     true
   end
 
-
+  def place_ship(boat)
+    @ship = boat
+  end
 end
