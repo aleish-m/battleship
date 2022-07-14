44c31c1496e995f973b5f7d9cf6ed572a89ed946
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-13 23:28:47 -0600

    test: checks for valid coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,6 +12,17 @@ describe Board do
   it "has cells as a hash" do
     expect(@board.cells).to be_a_kind_of(Hash)
     expect(@board.cells["A1"]).to be_instance_of(Cell)
+    expect(@board.cells.count).to be(16)
   end
 
+  it "has valid coordinates" do
+    expect(@board.valid_coordinate?("A1")).to eq(true)
+    expect(@board.valid_coordinate?("D4")).to eq(true)
+  end
+
+  it "checks for invalid coordinates" do
+    expect(@board.valid_coordinate?("A5")).to eq(false)
+  end
+
+  
 end
