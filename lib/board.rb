73dcad379983c57ac5171f439d5447a5d2f98df2
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-16 16:14:25 -0600

    fix: fix bugg on board class file

--- lib/board.rb
+++ lib/board.rb
@@ -30,57 +30,68 @@ class Board
 
   def valid_placement?(boat, coordinates)
 
+    row = coordinates.map do |coordinate|
+      coordinate[0]
+    end
+
+    num_row = row.map do |letter|
+      letter.ord
+    end
+
+    column = coordinates.map do |coordinate|
+      coordinate[1].to_i
+    end
+
+    overlap = coordinates.map do |coordinate|
+      cells[coordinate].empty?
+    end
+
     if coordinates.count == boat.length
-          row = coordinates.map do |coordinate|
-            coordinate[0]
-          end
 
-          num_row = row.map do |letter|
-            letter.ord
-          end
+      if overlap.all?
 
-          column = coordinates.map do |coordinate|
-            coordinate[1].to_i
-          end
-
-          overlap = coordinates.map do |coordinate|
-            cells[coordinate].empty?
-          end
-
-      if row.uniq.count == 1 &&
+        if row.uniq.count == 1 &&
           column.each_cons(2).all? do |num_1, num_2|
             num_2 == num_1 +1
           end
+
           true
 
-      elsif column.uniq.count == 1 &&
+        elsif column.uniq.count == 1 &&
           num_row.each_cons(2).all? do |num_1, num_2|
             num_2 == num_1 +1
           end
-            true
+
+          true
+
+        else
+          false
+        end
+
       else
         false
       end
+
     else
       false
     end
+
   end
 
-  def place(boat,coordinates)
-    coordinates.each do |coordinate|
-      @cells[coordinate].place_ship(boat)
-    end
-  end
 
-  def render
-    a_row = @cells.select do |coordinate, cell|
-      coordinate[0] == "A"
+    def place(boat,coordinates)
+      coordinates.each do |coordinate|
+        @cells[coordinate].place_ship(boat)
+      end
     end
 
-    "  1 2 3 4"
-    a_row.each do |coordinate, cell|
-      cell.render
+    def render(show = false)
+      "  1 2 3 4 \n" +
+      "A #{@cells.fetch_values("A1", "A2", "A3", "A4").map {|cell| cell.render(show)}.join(" ")} \n" +
+      "B #{@cells.fetch_values("B1", "B2", "B3", "B4").map {|cell| cell.render(show)}.join(" ")} \n" +
+      "C #{@cells.fetch_values("C1", "C2", "C3", "C4").map {|cell| cell.render(show)}.join(" ")} \n" +
+      "D #{@cells.fetch_values("D1", "D2", "D3", "D4").map {|cell| cell.render(show)}.join(" ")} \n"
+
     end
-  end
 
 end
